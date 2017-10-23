f847d4a32cf2dfa2d07b8dc5fa4eec2cc62c9746
amatiyko <andrew.matiyko@gmail.com>
2017-10-23 15:26:08 +0300

    .

--- js/script.js
+++ js/script.js
@@ -68,5 +68,26 @@ $(document).ready(function () {
     });
     return false;
   });
+
+
+
+
+
+    if($(window).width() <= 600){
+        // show menu on swipe to right
+        $(document).on('swiperight', function(e) {
+            e.preventDefault();
+            $('#menu').animate({
+                left: '0'
+            });
+        });
+        // hide menu on swipe to left
+        $(document).on('swipeleft',function(e){
+            e.preventDefault();
+            $('#menu').animate({
+                left: '-100%'
+            });
+        });
+    }
 });
 
