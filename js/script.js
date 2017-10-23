c456288e3b9f46706d260c1d255653e453c94780
amatiyko <andrew.matiyko@gmail.com>
2017-10-23 15:39:00 +0300

    .

--- js/script.js
+++ js/script.js
@@ -75,14 +75,14 @@ $(document).ready(function () {
 
 
         // show menu on swipe to right
-        $(document).on('swiperight', function(e) {
+        $('body').on('swiperight', function(se) {
             e.preventDefault();
             $('#menu').animate({
                 left: '0'
             });
         });
         // hide menu on swipe to left
-        $(document).on('swipeleft',function(e){
+        $('body').on('swipeleft',function(e){
             e.preventDefault();
             $('#menu').animate({
                 left: '-100%'
